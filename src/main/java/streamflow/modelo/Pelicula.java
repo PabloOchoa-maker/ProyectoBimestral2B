3bf79295f71f1b9c04a189ea33cefaccc7d5816e
psgd419 <pedrogomezdelgado03@gmail.com>
2026-07-14 16:18:14 -0500

    Segunda subida modelo

--- src/main/java/streamflow/modelo/Pelicula.java
+++ src/main/java/streamflow/modelo/Pelicula.java
@@ -2,4 +2,34 @@ package streamflow.modelo;
 
 public class Pelicula extends Contenido {
 
+    private String director;
+
+    public Pelicula(String id, String titulo, Genero genero, Calidad calidad,
+                    int duracionMin, double costoBase, String director) {
+        super(id, titulo, genero, calidad, duracionMin, costoBase);
+        this.director = director;
+    }
+
+    @Override
+    public void reproducir() {
+        System.out.println("Reproduciendo la pelicula: " + titulo + " (" + calidad + ")");
+    }
+
+    @Override
+    public String obtenerDetalles() {
+        return "Pelicula: " + titulo
+                + " | Director: " + director
+                + " | Genero: " + genero
+                + " | Duracion: " + duracionMin + " min"
+                + " | Calidad: " + calidad
+                + " | Costo: " + calcularCosto();
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
 }
--- src/main/java/streamflow/modelo/Suscripcion.java
+++ src/main/java/streamflow/modelo/Suscripcion.java
@@ -1,5 +1,40 @@
 package streamflow.modelo;
 
+import java.time.LocalDate;
+
 public class Suscripcion {
 
+    private String id;
+    private LocalDate fechaInicio;
+    private double costoMensual;
+
+    public Suscripcion(String id, LocalDate fechaInicio, double costoMensual) {
+        this.id = id;
+        this.fechaInicio = fechaInicio;
+        this.costoMensual = costoMensual;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public double getCostoMensual() {
+        return costoMensual;
+    }
+
+    public void setCostoMensual(double costoMensual) {
+        this.costoMensual = costoMensual;
+    }
 }
