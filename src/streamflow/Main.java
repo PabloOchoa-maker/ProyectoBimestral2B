3d5840f204d56cc0181ea52c4367351fdcac8226
PabloOchoa-maker <pablitoa8aj@gmail.com>
2026-07-16 19:03:43 -0500

    Cambio de Maven a Ant
    El proyecto pasa a ser Ant de NetBeans. Los paquetes de streamflow van
    ahora directo bajo src/, se anade streamflow.Main como punto de entrada
    y las dependencias (SQLite JDBC y slf4j-api) pasan a lib/ a mano.

---
+++ src/streamflow/Main.java
@@ -0,0 +1,14 @@
+package streamflow;
+
+/**
+ * Punto de entrada de StreamFlow.
+ *
+ * @author pablo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO: cablear vista, controlador, servicios y DAOs
+    }
+
+}
